a48e0e922a347b71243ac82cd6ffa798e025ac42
Joseph Mingrone <jrm@ftfl.ca>
2015-03-11 18:05:03 -0300

    Don't store shell commands in variables and don't use backticks

--- ml_vol.sh
+++ ml_vol.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-interval=3
-stump_pid=`pgrep -a -n stumpwm`
-vol_cmd="/usr/sbin/mixer -s vol | sed 's/vol [0-9]*://'"
+interval=3 # customize this
+stump_pid=$(pgrep -a -n stumpwm)
 
 # while stumpwm is still running
 while kill -0 $stump_pid > /dev/null 2>&1; do
-    echo `eval ${vol_cmd}`
+    echo $(/usr/sbin/mixer -s vol | sed 's/vol [0-9]*://')
     sleep ${interval}
 done
