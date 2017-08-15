fbb4a7b3062e69676d7613b32f2072563b555ff8
Joseph Mingrone <jrm@ftfl.ca>
2017-08-15 15:49:50 -0300

    Grab the StumpWM pid based on `sbcl .*/stmpwm/load.lisp`
    I'm now running StumpWM by loading it directly with sbcl, instead of the bundled
    executable.

--- ml_vol.sh
+++ ml_vol.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 interval=3 # customize this
-stump_pid="$(pgrep -a -n stumpwm)"
+
+# Set the variable stump_pid using one of these two lines.  Which line you use
+# depends on whether you run the large StumpWM executable that bundles SBCL, or
+# if you simply start SBCL and load StumpWM.  If you are using the FreeBSD
+# StumpWM package, use the second line.
+
+#stump_pid="$(pgrep -a -n stumpwm)"
+stump_pid="$(pgrep -anf -U "$(id -u)" "sbcl .*/stumpwm/load.lisp")"
 
 # while stumpwm is still running
 while kill -0 "$stump_pid" > /dev/null 2>&1; do
